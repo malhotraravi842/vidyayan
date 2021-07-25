2a360d71cd241e26a21139d5e7249b50b195d7f6
Ravi Chaudhary <malhotraravi842@gmail.com>
2021-07-25 12:33:40 +0530

    completed landing page

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// Get the new year 
+const getLaunchDate = () => {
+    return new Date('July 25 2021 12:31:00');
+}
+
+
+// select elements
+const app = document.querySelector('.countdown-timer');
+const message = document.querySelector('.message');
+const heading = document.querySelector('.title');
+const confetti = document.querySelector('#confetti');
+
+
+const format = (t) => {
+    return t < 10 ? '0' + t : t;
+};
+
+const render = (time) => {
+    app.innerHTML = `
+        <div class="count-down">
+            <div class="timer">
+                <h2 class="days">${format(time.days)}</h2>
+                <small>Days</small>
+            </div>
+            <div class="timer">
+                <h2>:</h2>
+            </div>
+            <div class="timer">
+                <h2 class="hours">${format(time.hours)}</h2>
+                <small>Hours</small>
+            </div>
+            <div class="timer">
+                <h2>:</h2>
+            </div>
+            <div class="timer">
+                <h2 class="minutes">${format(time.minutes)}</h2>
+                <small>Minutes</small>
+            </div>
+            <div class="timer">
+                <h2>:</h2>
+            </div>
+            <div class="timer">
+                <h2 class="seconds">${format(time.seconds)}</h2>
+                <small>Seconds</small>
+            </div>
+        </div>
+        `;
+};
+
+
+const countdownTimer = new CountDown(
+    getLaunchDate(),
+    render,
+);
---
+++ js/countdown.js
@@ -0,0 +1,52 @@
+class CountDown {
+    constructor(expiredDate, onRender) {
+        this.setExpiredDate(expiredDate);
+        this.onRender = onRender;
+    }
+
+    setExpiredDate(expiredDate) {
+        const currentTime = new Date().getTime();
+
+        this.timeRemaining = expiredDate.getTime() - currentTime;
+
+        this.timeRemaining > 0 ? this.start() : this.complete();
+    }
+
+    complete() {
+        heading.textContent = "Welcome To Vidyayan";
+        app.innerHTML = '';
+        message.style.display = 'initial';
+        animation();
+    }
+
+    getTime() {
+        return {
+            days: Math.floor(this.timeRemaining / 1000/ 60 / 60/ 24),
+            hours: Math.floor(this.timeRemaining / 1000/ 60 / 60)%24,
+            minutes: Math.floor(this.timeRemaining / 1000/ 60)%60,
+            seconds: Math.floor(this.timeRemaining / 1000)%60,
+        };
+    }
+
+    update() {
+        if(typeof this.onRender == 'function') {
+            this.onRender(this.getTime());
+        }
+    }
+
+    start() {
+
+        this.update();
+
+        const intervalId = setInterval(() => {
+            this.timeRemaining -= 1000;
+
+            if(this.timeRemaining < 0) {
+                this.complete();
+                clearInterval(intervalId);
+            }else {
+                this.update();
+            }
+        }, 1000);
+    }
+}
